95d78b7e67bca4c56541feb2d7172c516444e5ae
ibr14060 <104881960+ibr14060@users.noreply.github.com>
2022-12-15 16:45:43 +0200

    elshop kamel bl update

--- server/routes/record.js
+++ server/routes/record.js
@@ -10,13 +10,22 @@ const dbo = require("../db/conn");
  
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
  
  
+
+ 
+    
 // This section will help you get a list of all the records.
-recordRoutes.route("/record").get(function (req, res) {
- let db_connect = dbo.getDb("employees");
+//deh el sa7
+recordRoutes.route("/shop").get(function (req, res) {
+ let db_connect = dbo.getDb("worldcup22");
+ //let ticketss=[]
  db_connect
-   .collection("records")
+   .collection("Shop")
    .find({})
    .toArray(function (err, result) {
      if (err) throw err;
@@ -24,12 +33,174 @@ recordRoutes.route("/record").get(function (req, res) {
    });
 });
  
+//search by awayteam and hometeam
+recordRoutes.route("/shop/:id").get(function(req, res) {
+  let db_connect = dbo.getDb("worldcup22");
+      db_connect
+      .collection("Shop")
+      .find({
+        $or: [
+          {
+            "AwayTeam": req.params.id,
+          },
+          {
+            "HomeTeam": req.params.id,
+          }
+        ],
+      }).toArray((err,result)=>{
+        if (err) throw err;
+        res.json(result);
+      });
+});
+// delete ticket by id
+recordRoutes.route("/shop/delete/:id").delete((req, response) => {
+  let db_connect = dbo.getDb("worldcup22");
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("Shop").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+ });
+ recordRoutes.route("/shop/update/:id").post(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      MatchNumber: req.body.MatchNumber,
+      
+    },
+  };
+  db_connect
+    .collection("shop")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+ });
+ // decrement the number of tickets
+ recordRoutes.route("/update/:id/:cno/:capacity").patch(function (req, response) {
+let decrement=Number(req.params.capacity)*-1
+let db_connect = dbo.getDb("worldcup22");
+let myquery = { "_id": ObjectId(req.params.id),
+"availability.category1.price":75 };
+let newvalues = {
+  $inc: {
+"availability.category1.count":decrement
+  },
+};
+if (Number(req.params.cno)==1){
+  myquery = { "_id": ObjectId(req.params.id),
+"availability.category1.price":75 };
+newvalues = {
+  $inc: { "availability.category1.count":decrement
+  },
+};
+}
+else if(Number(req.params.cno)==2){
+  myquery = { "_id": ObjectId(req.params.id),
+"availability.category2.price":125 };
+newvalues = {
+  $inc: {
+"availability.category2.count":decrement
+  },
+};
+}
+else {
+  myquery = { "_id": ObjectId(req.params.id),
+"availability.category3.price":195 };
+newvalues = {
+  $inc: {
+"availability.category3.count":-1
+  },
+};
+}
+//console.log(Number(myquery._id.availability.category1.count))
+
+db_connect
+.collection("shopMasterlist")
+.updateOne(myquery, newvalues, function (err, res) {
+if (err) throw err;
+console.log("1 document updated for reservation");
+response.json(res);
+});
+});
+// increment the number of tickets
+recordRoutes.route("/cancel/:id/:cno/:capacity").patch(function (req, response) {
+  let increment=Number(req.params.capacity)
+let db_connect = dbo.getDb("worldcup22");
+let myquery = { "_id": ObjectId(req.params.id),
+"availability.category1.price":75 };
+let newvalues = {
+  $inc: {
+"availability.category1.count":increment
+  },
+};
+if (Number(req.params.cno)==1){
+  myquery = { "_id": ObjectId(req.params.id),
+"availability.category1.price":75 };
+newvalues = {
+  $inc: {
+"availability.category1.count":increment
+  },
+};
+}
+else if(Number(req.params.cno)==2){
+  myquery = { "_id": ObjectId(req.params.id),
+"availability.category2.price":125 };
+newvalues = {
+  $inc: {
+"availability.category2.count":increment
+  },
+};
+}
+else {
+  myquery = { "_id": ObjectId(req.params.id),
+"availability.category3.price":195 };
+newvalues = {
+  $inc: {
+"availability.category3.count":increment
+  },
+};
+}
+//console.log(Number(myquery._id.availability.category1.count))
+
+db_connect
+.collection("shopMasterlist")
+.updateOne(myquery, newvalues, function (err, res) {
+if (err) throw err;
+console.log("1 document updated for reservation");
+response.json(res);
+});
+});
+/*
+recordRoutes.route("/shop/delete/:id").get(function(req, res) {
+  let db_connect = dbo.getDb("worldcup22");
+      db_connect
+      .collection("Shop")
+      .find({
+        $or: [
+          {
+            "AwayTeam": req.params.id,
+          },
+          {
+            "HomeTeam": req.params.id,
+          }
+        ],
+      }).toArray((err,result)=>{
+        if (err) throw err;
+        res.json(result);
+      });
+});
+*/
+/*
 // This section will help you get a single record by id
-recordRoutes.route("/record/:id").get(function (req, res) {
- let db_connect = dbo.getDb();
+recordRoutes.route("/shop/:id").get(function (req, res) {
+ let db_connect = dbo.getDb("worldcup22");
  let myquery = { _id: ObjectId(req.params.id) };
  db_connect
-   .collection("records")
+   .collection("Shop")
    .findOne(myquery, function (err, result) {
      if (err) throw err;
      res.json(result);
@@ -37,7 +208,7 @@ recordRoutes.route("/record/:id").get(function (req, res) {
 });
  
 // This section will help you create a new record.
-recordRoutes.route("/record/add").post(function (req, response) {
+recordRoutes.route("/shop/add").post(function (req, response) {
  let db_connect = dbo.getDb();
  let myobj = {
    name: req.body.name,
@@ -50,7 +221,7 @@ recordRoutes.route("/record/add").post(function (req, response) {
  });
 });
  
-// This section will help you update a record by id.
+ This section will help you update a record by id.
 recordRoutes.route("/update/:id").post(function (req, response) {
  let db_connect = dbo.getDb();
  let myquery = { _id: ObjectId(req.params.id) };
@@ -80,5 +251,5 @@ recordRoutes.route("/:id").delete((req, response) => {
    response.json(obj);
  });
 });
- 
+*/ 
 module.exports = recordRoutes;
--- shop.js
+++ shop.js
@@ -1,2 +1,24 @@
-const member1="bugDigger0"
-console.log(member1)
+//const member1="bugDigger0"
+const express = require("express");
+const { appendFile } = require("fs");
+const { DefaultDeserializer } = require("v8");
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//console.log(member1)
+app.get('/shop',(req,res)=>{
+ let tickets =[] 
+db.collection('shop')
+.find ()
+.sort({MatchNumber:1})
+.foreach(shop=>tickets.push(shop))
+.then(()=>{
+    res.status(200).json(tickets)
+})
+.catch(()=>{
+    res.status(500).json({error :'could not fetch the documents'})
+})
+
+})
